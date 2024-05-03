d34469ceeff43a4d674528103a9538dcd1458ce6
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-05-03 21:59:32 +0700

    Solved 7

---
+++ Exercise 7: Add new item to list after a specified item.py
@@ -0,0 +1,16 @@
+# Exercise 7: Add new item to list after a specified item
+# Write a program to add item 7000 after 6000 in the following Python List
+
+# Given:
+
+# list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+
+# Expected output:
+
+# [10, 20, [300, 400, [5000, 6000, 7000], 500], 30, 40]
+def main():
+    list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+    list1[2][2].append(7000)
+    print(list1)
+if __name__=="__main__":
+    main()
